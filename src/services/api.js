e1dbfa3b8b5f81ef82c952d0efe2594a9a783759
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-09-08 16:55:04 -0300

    requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const data = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const resposta = await data.json();
+  return resposta;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const resposta = await data.json();
+  return resposta;
 }
 
 export async function getProductById() {
