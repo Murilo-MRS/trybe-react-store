dc76198fcd10f16c6a4c47148a2442e9f37627d3
Marco Viana <marcoviana100@gmail.com>
2022-09-08 18:48:28 -0300

    REQUISITO 3 - CONCLUÍDO 2.0

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
-import Carrinho from './pages/Carrinho'
+import Carrinho from './pages/Carrinho';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Home } />
-        <Route exact path="/carrinho" component={ Carrinho }/>
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/carrinho" component={ Carrinho } />
+        </Switch>
       </BrowserRouter>
     );
   }
