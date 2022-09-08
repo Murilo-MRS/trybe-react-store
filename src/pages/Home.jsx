5373356e52f1694df49b3bed5826a2cffe2a4a40
Murilo-MRS <murilosantana1993@gmail.com>
2022-09-08 16:52:52 -0300

    REQUISITO 2 - CONCLUIDO

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
