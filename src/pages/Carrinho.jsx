81c2a4284030ec23c08fd45867b6982e4f0b45b7
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-09-13 18:30:30 -0300

    requisito 9

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
+import { getCartList } from '../services/api';
 
 class Carrinho extends Component {
+  state = {
+    cartList: [],
+  };
+
+  componentDidMount() {
+    this.reviewCartList();
+  }
+
+  reviewCartList = () => {
+    const cartListFromStorage = getCartList();
+    this.setState({ cartList: cartListFromStorage }, () => {
+      if (cartListFromStorage.length > 0) {
+        this.setState({ cartList: cartListFromStorage });
+      }
+      console.log(cartListFromStorage);
+    });
+  };
+
   render() {
+    const { cartList } = this.state;
+    console.log(cartList);
     return (
       <div>
-        <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+        {cartList.length > 0 ? (
+          cartList.map((e) => (
+            <div key={ e.id }>
+              <p data-testid="shopping-cart-product-name">
+                {e.title}
+              </p>
+              <p data-testid="shopping-cart-product-quantity">{e.qtd}</p>
+              <p>
+                { e.price }
+              </p>
+            </div>
+          ))
+        ) : (
+          <h2 data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </h2>
+        )}
       </div>
     );
   }
